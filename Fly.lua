b218b1423611f5180b4cd49d878538b7ed43d2ef
senhorGuest6 <bloxbloxberryzinnblox@gmail.com>
2025-07-06 15:12:28 -0300

    Update Fly.lua

--- Fly.lua
+++ Fly.lua
@@ -1,4 +1,5 @@
--- MP HUB FLY SCRIPT
+-- MP HUB - Fly Script com ativar/desativar (pressione F)
+
 local UIS = game:GetService("UserInputService")
 local player = game.Players.LocalPlayer
 local char = player.Character or player.CharacterAdded:Wait()
@@ -12,24 +13,25 @@ UIS.InputBegan:Connect(function(key, gp)
     if key.KeyCode == Enum.KeyCode.F then
         flying = not flying
         if flying then
+            -- Ativar voo
             bv = Instance.new("BodyVelocity")
-            bv.Velocity = Vector3.new(0,0,0)
-            bv.MaxForce = Vector3.new(9e9,9e9,9e9)
+            bv.Velocity = Vector3.new(0, 0, 0)
+            bv.MaxForce = Vector3.new(9e9, 9e9, 9e9)
             bv.Parent = hrp
 
             bg = Instance.new("BodyGyro")
-            bg.MaxTorque = Vector3.new(9e9,9e9,9e9)
+            bg.MaxTorque = Vector3.new(9e9, 9e9, 9e9)
             bg.CFrame = hrp.CFrame
             bg.Parent = hrp
 
-            while flying do
-                task.wait()
-                bv.Velocity = workspace.CurrentCamera.CFrame.LookVector * 50
+            while flying and task.wait() do
+                bv.Velocity = workspace.CurrentCamera.CFrame.LookVector * 60
                 bg.CFrame = workspace.CurrentCamera.CFrame
             end
         else
+            -- Desativar voo
             if bv then bv:Destroy() end
             if bg then bg:Destroy() end
         end
     end
-end)# Mp-hub-v9999786858_76
+end)
